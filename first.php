b775600b1e987a5f5ef864f0e0b19c731d513444
LIM YOUN SOO <maekung3@gmail.com>
2017-09-06 16:33:09 +0900

    Bootstrap(css, js), JQuery setting

---
+++ first.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang = "ko">
+  <head>
+    <title>Tap The World</title>
+
+<!-- Add meta Tag for Responsive Design => Can control with Device's width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"  charset="utf-8">
+<!-- Add Bootstrap Css, Js & jquery -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+  </head>
+
+
+<body>
+
+<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAwrfz2MVZoTgp-8XvRRrdSyba3_gV_4VU&callback=initMap"></script>
+
+<script src = "http://malsup.github.io/jquery.cycle2.js?ver=1"></script>
+</body>
+
+
+</html>
